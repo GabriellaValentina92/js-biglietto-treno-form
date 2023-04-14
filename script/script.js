dec8913c9b7375712acff6e84c81dcb349f04e74
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-14 09:29:39 +0200

    add fuction

--- script/script.js
+++ script/script.js
@@ -1,33 +1,34 @@
 // variabili globali
-
 let Name = document.getElementById('#first_lastname');
 let km_number = document.querySelector('#distance_km'); 
 let age = document.querySelector('#age');
 let btnGenerator = document.querySelector('.btn-ticket');
 let btnDelete = document.querySelector('.btn-delete');
 
-// calcolo ticket
-let price = (km_number * 0.21);
 
-let discountYoung = price - (price * 20 / 100);
-let discountaged = price - (price * 40 / 100);
+btnGenerator.addEventListener('click' , function(){
+    
+    let Name = document.getElementById('#first_lastname');
+    let km_number = document.querySelector('#distance_km'); 
+    let age = document.querySelector('#age').value;
+    
+    // calcolo ticket
+    let price = (km_number.value * 0.21);
+    
+    let discountYoung = price - (price * 20 / 100);
+    let discountaged = price - (price * 40 / 100);
+    
+    let discount = discountYoung.toFixed(2)
+    let Discount = discountaged.toFixed(2)
+    
+    
+    if (age === under 18){
+        discountYoung = price - (price * 20 / 100)
+    } else if (age === over 65){
+        discountaged = price - (price * 40 / 100)
+    }else {
+        price = (km_number * 0.21)
+    }
+})
 
-let discount = discountYoung.toFixed(2)
-let Discount = discountaged.toFixed(2)
-
-
-if (age < 18){
-    btnGenerator.innerHTML = discount;
-} else if (age > 65){
-    btnGenerator.innerHTML = Discount;
-}else {
-    btnGenerator.innerHTML = price;
-}
-
-// const btn = function (){
-//     document.querySelector('.btn-ticket').addEventListener ('click' , ticket-price);
-// }
-
-// const Btn = function (){
-//     document.querySelector('.btn-delete').addEventListener ('click' , reset);
-// }
+document.getElementById('#first_lastname').innerHTML = 'il tuo nome è:  
